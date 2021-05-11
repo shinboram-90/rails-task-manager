04e74e2f0ced54ec9e7828d800fa742592a6c530
Lisa Chatellier <lisachatellier90@gmail.com>
2021-05-12 01:13:23 +0200

    keep going

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_095723) do
+ActiveRecord::Schema.define(version: 2021_05_11_102758) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
